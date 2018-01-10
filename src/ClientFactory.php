7d9818b4b19f55cc8563290d6d18c054d8d3ab2d
Richard Hagen <richard.hagen@gmail.com>
2018-01-10 10:13:50 +0100

    created a Client factory

---
+++ src/ClientFactory.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: richard
+ * Date: 10.01.18
+ * Time: 10:10
+ */
+
+namespace LightningSale\LndClient;
+
+
+class ClientFactory
+{
+    public static function createRestClient(string $lndHost, string $lndRestPort, string $rpcUsername, string $rpcPassword, string $lndCertificateFile): RestClient
+    {
+        $client = new \GuzzleHttp\Client([
+            'base_uri' => "https://$rpcUsername:$rpcPassword@$lndHost:$lndRestPort",
+            'verify' => $lndCertificateFile,
+        ]);
+
+        return new RestClient($client);
+    }
+}
