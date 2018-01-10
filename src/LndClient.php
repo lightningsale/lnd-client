8e886f179308d11ce98b5940bb16ec28cb7ffd7e
Richard Hagen <richard.hagen@gmail.com>
2018-01-10 08:34:32 +0100

    move LndClient out of Resource namespace

--- src/LndClient.php
+++ src/LndClient.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace LightningSale\LndRest\Resource;
+namespace LightningSale\LndRest;
 
 use GuzzleHttp\Client;
 use LightningSale\LndRest\Model\ChannelFeeReport;
